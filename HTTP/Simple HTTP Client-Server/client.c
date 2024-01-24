f47f288c9f0a0da9090fe62c32584287962925b9
Ajay-Kumaar <ajay@mobicip.com>
2024-01-24 14:39:24 +0530

    Modified the HTTP server to respond to specific resource request

--- HTTP/Simple HTTP Client-Server/client.c
+++ HTTP/Simple HTTP Client-Server/client.c
@@ -3,42 +3,58 @@
 #include<string.h>
 #include<strings.h>
 #include<unistd.h>
-#include<time.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+
+#define LOCALHOST "127.0.0.1"
+#define PORT 8080
+#define MAXSIZE 1024
 
 int main (int argc, char* argv[])
 {
 	int socket_client;
 	/*if(argc != 2)
 	{
-		printf ("\nInvalid format...Expected format: program_name IP_Address_of_the_Server");
+		printf ("\nInvalid format...Expected format: program_name IP_Address_of_the_Server"); This is for explicit Server IP address provided in the command line
 		exit(1);
 	}*/
 	if((socket_client = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf ("\nSocket cannot be opened.");
+		printf ("Socket cannot be opened...\n");
 		exit(1);
 	}
+	printf ("\nSocket opened successfully...\n");
 	struct sockaddr_in server_address;
 	bzero(&server_address, sizeof(server_address));
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(8000);
-	//inet_pton(AF_INET, argv[1], &server_address.sin_addr);
+	if(argc == 1)
+	{
+		server_address.sin_port = htons(8080);
+		server_address.sin_addr.s_addr = inet_addr(LOCALHOST);
+	}
+	else
+	{
+		server_address.sin_port = htons(80);
+		inet_pton(AF_INET, argv[1], &server_address.sin_addr);
+	}
 	if(connect(socket_client, (struct sockaddr*) &server_address, sizeof(server_address)) < 0)
 	{
-		printf ("\nConnection Failed.");
+		printf ("Connection failed...\n");
 		exit(1);
 	}
-	printf ("\nClient-Server connected...\n");
-	char request[100] = "GET / HTTP/1.1\r\n\r\n";
-	char response[20000];
-	write(socket_client,request,100);
-	bzero(response,20000);
-	read(socket_client,response,20000);
-	printf("\nResponse from the Server: %s\n",response);
+	printf("Client connected successfully to the Server %s:%d\n",inet_ntoa(server_address.sin_addr),ntohs(server_address.sin_port));
+	char request[] = "GET / HTTP/1.1\r\n\r\n";
+	char response[MAXSIZE];
+	bzero(response,MAXSIZE);
+	write(socket_client,request,sizeof(request));
+	printf("\nResponse from the Server:\n\n");
+	while(read(socket_client,response,MAXSIZE) > 0)
+	{
+		printf("%s",response);
+		bzero(response,MAXSIZE);
+	}
 	close(socket_client);
 	return 0;
 }
--- HTTP/Simple HTTP Client-Server/server.c
+++ HTTP/Simple HTTP Client-Server/server.c
@@ -8,6 +8,9 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 
+#define LOCALHOST "127.0.0.1"
+#define PORT 8080
+
 int main (int argc, char* argv[])
 {
 	FILE* fp = fopen("index.html","r");
@@ -15,33 +18,41 @@ int main (int argc, char* argv[])
 	fgets(response,1000,fp);
 	fclose(fp);
 	//char header[2000] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-	char header[2000] = "HTTP/1.1 200 OK\r\n\n";	
+	char header[1024] = "HTTP/1.1 200 OK\r\n\n";	
 	strcat(header,response);
 	int socket_server,socket_client;
 	if((socket_server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf ("\nSocket cannot be opened.");
+		printf ("Socket cannot be opened...\n");
 		exit(1);
 	}
+	printf ("\nSocket opened successfully...\n");
 	struct sockaddr_in server_address;
 	bzero(&server_address, sizeof(server_address));
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(8000);
-	server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = inet_addr(LOCALHOST);
 	if((bind(socket_server,(struct sockaddr*) &server_address, sizeof(server_address))) < 0)
 	{
-		printf("\nServer Bind failed.");
+		printf("Server Bind failed...\n");
 		exit(1);
 	}
-	listen(socket_server,5);
+	printf("Server bind success...\n");
+	if(listen(socket_server,5) < 0)
+	{
+		printf("Server listen function failed...\n");
+		exit(1);
+	}
+	printf("Server listening at port: %d\n", PORT);
 	while(1)
 	{
 		if((socket_client = accept(socket_server, NULL, NULL)) < 0)
 		{
-			printf("\nClient is Bad.");
+			printf("Connection to Client failed...\n");
 			exit(1);
 		}
-		write(socket_client, header, 2000);
+		printf("Connection to Client successful...\n");
+		write(socket_client, header, sizeof(header));
 		close(socket_client);
 	}
 	return 0;
