5e2a57e116d4e263297afeb1ced29d8a98e5cdd5
Ajay-Kumaar <ajay@mobicip.com>
2024-02-06 19:17:27 +0530

    Modified the Tic-Tac-Toe client and server programs

--- Tic-Tac-Toe Game/client.c
+++ Tic-Tac-Toe Game/client.c
@@ -1,76 +1,99 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<strings.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #define PORT 8080
-#define BOARD_SIZE 3
 
-void print_board(char board[BOARD_SIZE][BOARD_SIZE])
+void displayBoard(char board[3][3])
 {
-    for (int i = 0; i < BOARD_SIZE; ++i)
+    printf("Current Tic-Tac-Toe Board:\n");
+    printf("-------------\n");
+    for (int i = 0; i < 3; i++)
 	{
-        for (int j = 0; j < BOARD_SIZE; ++j)
-		{
-            printf(" %c ", board[i][j]);
-            if (j < BOARD_SIZE - 1)
-				printf("|");
-        }
-        printf("\n");
-        if (i < BOARD_SIZE - 1)
-			printf("-----------\n");
+        for (int j = 0; j < 3; j++)
+            printf("| %c ", board[i][j]);
+        printf("|\n-------------\n");
     }
-    printf("\n");
+}
+void playGame(int socket,int player_id)
+{
+    int move,row,col,gameover=0,currentPlayer = 0;
+    char board[3][3];
+    recv(socket, board, sizeof(board), 0);
+    displayBoard(board);
+	while(1)
+	{
+		recv(socket, &currentPlayer , sizeof(int), 0);
+		if(currentPlayer == player_id)
+		{
+			printf("Your turn\n\n");
+			while(1)
+			{
+				printf("Enter your move (0-8): ");
+				scanf("%d", &move);
+				int row = move / 3;
+		    	int col = move % 3;
+				if((move<0 || move>8) || (board[row][col] != ' '))
+				{
+					printf("Invalid move!\n");
+					continue;
+				}
+		        board[row][col] = (currentPlayer == 0) ? 'X' : 'O';
+				break;
+			}
+			send(socket, board, sizeof(board), 0);
+		}
+		else
+		{
+			printf("Not your turn, please wait for any further updates.\n");
+			recv(socket, board, sizeof(board), 0);
+		}
+		printf("Current Tic-Tac-Toe Board:\n");
+		displayBoard(board);
+		recv(socket, &gameover , sizeof(int), 0);
+		if(gameover == 1)
+		{
+			int win_player_id = 0;
+			recv(socket,&win_player_id,sizeof(int),0);
+			if(player_id == win_player_id)
+				printf("\n\nYou won the Game!!!\n\n");
+			else
+				printf("\n\nBetter Luck next time\n\n");
+			return;
+		}
+		else if(gameover == 2)
+			printf("It's a tie!!!\n\n");
+    }
 }
 int main()
 {
-    int client_socket;
+    int client_socket,player_id = 0;;
     struct sockaddr_in server_addr;
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1)
+    if(client_socket == -1)
 	{
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
-	printf("Client Socket created successfully\n");
+	printf("Client socket created successfully\n");
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+    if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
 	{
         perror("Error connecting to server");
         exit(EXIT_FAILURE);
     }
-	printf("Client connected to the Server successfully\n");
-    char board[BOARD_SIZE][BOARD_SIZE];
-    int game_status = 0;
-    while (1)
-	{
-        recv(client_socket, board, sizeof(board), 0);
-        printf("Current Tic-Tac-Toe Board:\n");
-        print_board(board);
-        recv(client_socket, &game_status, sizeof(int), 0);
-        if (game_status == 1)
-		{
-            printf("Game over. You win!\n");
-            break;
-        }
-		else if (game_status == -1)
-		{
-            printf("Game over. It's a tie!\n");
-            break;
-        }
-        int row, col;
-        printf("\nEnter row and column numbers (separated by space): ");
-        scanf("%d %d", &row, &col);
-        send(client_socket, &row, sizeof(int), 0);
-        send(client_socket, &col, sizeof(int), 0);
-    }
+    printf("Client successfully connected to the Server\n");
+	recv(client_socket, &player_id, sizeof(int), 0);
+	printf("PlayerID: %d\n\n",player_id);
+	while(1)
+    	playGame(client_socket,player_id);
     close(client_socket);
     return 0;
 }
--- Tic-Tac-Toe Game/server.c
+++ Tic-Tac-Toe Game/server.c
@@ -1,127 +1,136 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<strings.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #define PORT 8080
 #define MAX_CLIENTS 2
-#define BOARD_SIZE 3
 
-void print_board(char board[BOARD_SIZE][BOARD_SIZE])
+void displayBoard(char board[3][3])
 {
-    for (int i = 0; i < BOARD_SIZE; ++i)
+    printf("Current Tic-Tac-Toe Board:\n");
+    printf("-------------\n");
+    for (int i = 0; i < 3; i++)
 	{
-        for (int j = 0; j < BOARD_SIZE; ++j)
-		{
-            printf(" %c ", board[i][j]);
-            if (j < BOARD_SIZE - 1)
-				printf("|");
-        }
-        printf("\n");
-        if (i < BOARD_SIZE - 1)
-			printf("-----------\n");
+        for (int j = 0; j < 3; j++)
+            printf("| %c ", board[i][j]);
+        printf("|\n-------------\n");
     }
-    printf("\n");
 }
-int check_winner(char board[BOARD_SIZE][BOARD_SIZE])
+int checkWin(char board[3][3])
 {
-    for (int i = 0; i < BOARD_SIZE; ++i)
+    for(int i=0;i<3;i++)
 	{
-        if ((board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') ||
-            (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' '))
+        if(board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            return 1;
+        if(board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i])
             return 1;
     }
-    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') ||
-        (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' '))
+    if(board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
         return 1;
-    for (int i = 0; i < BOARD_SIZE; ++i)
+    if(board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        return 1;
+    return 0;
+}
+int checkDraw(char board[3][3])
+{
+    for(int i=0;i<3;i++)
 	{
-        for (int j = 0; j < BOARD_SIZE; ++j)
+        for(int j=0;j<3;j++)
 		{
-            if (board[i][j] == ' ')
+            if(board[i][j] == ' ')
                 return 0;
         }
     }
-    return -1;
+    return 1;
 }
-int is_valid_move(char board[BOARD_SIZE][BOARD_SIZE], int row, int col)
+void playGame(int client_sockets[MAX_CLIENTS])
 {
-    return (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE && board[row][col] == ' ');
+    char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    int currentPlayer = 0,gameover = 0,move;
+	for(int i=0;i<MAX_CLIENTS;i++)
+		send(client_sockets[i], board, sizeof(board), 0);
+    while(!gameover)
+	{
+		for(int i=0;i<MAX_CLIENTS;i++)
+			send(client_sockets[i], &currentPlayer, sizeof(int), 0);
+        recv(client_sockets[currentPlayer], board, sizeof(board), 0);
+		send(client_sockets[1-currentPlayer], board, sizeof(board), 0);
+        if(checkWin(board))
+		{
+            printf("\n\nWohoo!! Player %d wins!\n", currentPlayer);
+            gameover = 1;
+        }
+		else if(checkDraw(board))
+		{
+            printf("It's a draw!\n");
+            gameover = 2;
+        }
+		else
+			gameover = 0;
+		for(int i=0;i<MAX_CLIENTS;i++)
+		{
+			send(client_sockets[i], &gameover, sizeof(int), 0);
+			if(gameover == 1)
+				send(client_sockets[i], &currentPlayer, sizeof(int), 0);
+		}
+        currentPlayer = 1-currentPlayer;
+    }
 }
 int main()
 {
-    int server_socket, client_sockets[MAX_CLIENTS];
 	int yes = 1;
+    int server_socket, client_sockets[MAX_CLIENTS];
     struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_size = sizeof(struct sockaddr_in);
+    socklen_t addr_size = sizeof(client_addr);
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1)
+    if(server_socket == -1)
 	{
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
-	printf("\nServer Socket created successfully\n");
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+	if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+	{
 		perror("setsockopt");
 		exit(0);	
 	}
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+	printf("Server socket created successfully\n");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    if(bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
 	{
         perror("Error binding socket");
         exit(EXIT_FAILURE);
     }
 	printf("Server bind successful\n");
-    if (listen(server_socket, MAX_CLIENTS) == -1)
+    if(listen(server_socket, MAX_CLIENTS) == -1)
 	{
         perror("Error listening for connections");
         exit(EXIT_FAILURE);
     }
     printf("Server is listening at port %d\n\n", PORT);
-    for (int i = 0; i < MAX_CLIENTS; ++i)
+    for(int i=0;i<MAX_CLIENTS;i++)
 	{
         client_sockets[i] = accept(server_socket, (struct sockaddr*)&client_addr, &addr_size);
-        if (client_sockets[i] == -1)
-		{
-            perror("Error accepting connection");
-            exit(EXIT_FAILURE);
-        }
-        printf("Client %d connected\n", i+1);
+        printf("Client %d connected.\n", i+1);
     }
-    char board[BOARD_SIZE][BOARD_SIZE];
-    memset(board, ' ', sizeof(board));
-    int current_client = 0;
-    int game_status = 0;
-    while (1)
+	// Creating id for each player and sending it to them
+	for(int i=0;i<MAX_CLIENTS;i++)
+		send(client_sockets[i], &i, sizeof(int), 0);
+	while(1)
 	{
-		for (int i = 0; i < MAX_CLIENTS; ++i)
-            send(client_sockets[i], board, sizeof(board), 0);
-		for (int i = 0; i < MAX_CLIENTS; ++i)
-            send(client_sockets[i], &game_status, sizeof(int), 0);
-        int row, col;
-        recv(client_sockets[current_client], &row, sizeof(int), 0);
-        recv(client_sockets[current_client], &col, sizeof(int), 0);
-        if (is_valid_move(board, row, col))
-		{
-            board[row][col] = (current_client == 0) ? 'X' : 'O';
-            game_status = check_winner(board);
-            if (game_status != 0)
-			{
-                for (int i = 0; i < MAX_CLIENTS; ++i)
-                    send(client_sockets[i], &game_status, sizeof(int), 0);
-                break;
-            }
-        }
-        current_client = 1-current_client;
-    }
-    for (int i = 0; i < MAX_CLIENTS; ++i)
+		int start = 0;
+		printf("\nDo you want to start a new Game (1/0): ");
+		scanf("%d",&start);
+		if(start)
+    		playGame(client_sockets);
+	}
+    for(int i=0;i<MAX_CLIENTS;i++)
         close(client_sockets[i]);
     close(server_socket);
     return 0;
