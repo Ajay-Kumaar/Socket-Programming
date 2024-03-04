ca1192666f1c8c861cc42bb9cff14c23b59298ae
Ajay-Kumaar <ajay@mobicip.com>
2024-03-04 19:11:00 +0530

    Working on server code

--- Tic-Tac-Toe Game/WebSockets/server.c
+++ Tic-Tac-Toe Game/WebSockets/server.c
@@ -4,13 +4,17 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <pthread.h>
 
 #define PORT 8000
-#define MAX_BUFFER_SIZE 1024
 #define MAX_CLIENTS 2
+#define MAX_BUFFER_SIZE 1024
 
+int p = 0;
 int client_sockets[MAX_CLIENTS];
 char board[3][3];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t player_id = 0;
 
 void initialize_board(char board[3][3])
 {
@@ -63,6 +67,75 @@ void display_board(char board[3][3])
     }
 }
 
+void handle_player(int client_socket, int player_id)
+{
+	initialize_board(board);
+    char buffer[MAX_BUFFER_SIZE];
+    ssize_t bytes_received = read(client_socket, buffer, sizeof(buffer));
+    printf("\nWebSocket Handshake request received from the player %d is:\n\n%s\n\n", player_id, buffer);
+    if (bytes_received > 0)
+	{
+        char* key_start = strstr(buffer, "Sec-WebSocket-Key: ") + 19;
+        char* key_end = strchr(key_start, '\r');
+        *key_end = '\0';
+        char key[128];
+        char magic_string[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+        strcpy(key, key_start);
+        strcat(key, magic_string);
+        printf("\nThe Concatenated Key is: %s.\n\n", key);
+        static unsigned char sha1Hash[SHA_DIGEST_LENGTH];
+        SHA1((const unsigned char *)concatenatedKey, strlen(concatenatedKey), sha1Hash);
+        printf("\n\nSHA1 is: %s.\n\n", sha1Hash);
+        BIO *bmem = BIO_new(BIO_s_mem());
+        BIO *b64 = BIO_new(BIO_f_base64());
+        BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+        BIO_push(b64, bmem);
+        BIO_write(b64, sha1Hash, SHA_DIGEST_LENGTH);
+        BIO_flush(b64);
+        char* base64Hash = (char *)malloc(40);
+        BUF_MEM *bptr;
+        BIO_get_mem_ptr(b64, &bptr);
+        strcpy(base64Hash, bptr->data);
+        int length = strlen(base64Hash);
+        if (length > 0 && base64Hash[length-1] == '\n')
+            length--;
+        printf("\n\nThe base is: %s\n\n", base64Hash);
+        BIO_free_all(b64);
+        const char* response_headers = "HTTP/1.1 101 Switching Protocols\r\n"
+                        			   "Upgrade: websocket\r\n"
+                        			   "Connection: Upgrade\r\n"
+                        			   "Sec-WebSocket-Accept: %.*s\r\n"
+                       				   "\r\n";
+        char response[256];
+        snprintf(response, sizeof(response), response_headers, length, base64Hash);
+        write(client_socket, response, strlen(response));
+    }
+}
+
+void* handle_thread(void* client_socket_ptr)
+{
+	printf("\n\nIn handle_thread function.\n\n");
+	printf("\n\n%ld is the player number.\n\n",player_id);
+	int client_socket = *((int*)client_socket_ptr);
+	if (player_id < MAX_CLIENTS)
+	    client_sockets[player_id] = client_socket;
+	else
+	{
+		perror("Exceeded the maximum number of players.\n");
+		exit(-1);
+	}
+	pthread_t currentPlayerNumber = __sync_add_and_fetch(&player_id, 1);
+	handle_player(client_socket, player_id);
+	//handle_player(client_socket, currentPlayerNumber);
+	for (int i = 0; i < player_id; i++) 
+	{
+	     close(client_sockets[i]);
+	     printf("\n\nClient Socket %d is closed.\n\n", i);
+	}
+	free(client_socket_ptr);
+	pthread_exit(NULL);
+}
+
 int main()
 {
 	int server_socket, client_socket, yes = 1;
@@ -99,7 +172,16 @@ int main()
 		{
             perror("Error in accepting the client connection.\n");
             continue;
-        }        
+        }
+		pthread_t thread_id;
+        int* client_socket_ptr = malloc(sizeof(int));
+        *client_socket_ptr = client_socket;
+        if (pthread_create(&thread_id, NULL, handle_thread, client_socket_ptr) != 0)
+		{
+            perror("Error in creating the thread.\n");
+            close(client_socket);
+            free(client_socket_ptr);
+        }    
     }
     close(server_socket);
     return 0;
